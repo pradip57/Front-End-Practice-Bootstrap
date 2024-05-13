9980170af1bd94742ac5905e8a6980a72922ad5d
Pradip <dellpidarp57@gmail.com>
2024-05-13 09:03:53 +0545

    making dynamic, dom manipulation

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const courses = [
+  {
+    id: 1,
+    name: "Web Design Training",
+    duration: "2.5 months",
+    image: "./images/img3.webp",
+  },
+  {
+    id: 2,
+    name: "Python Training",
+    duration: "2.5 months",
+    image: "./images/img4.webp",
+  },
+  {
+    id: 3,
+    name: "Ethical Hacking Training",
+    duration: "3 months",
+    image: "./images/img5.webp",
+  },
+  {
+    id: 4,
+    name: "Business Analysis Training",
+    duration: "2 months",
+    image: "./images/img6.webp",
+  },
+  {
+    id: 5,
+    name: "Digital Marketing Training",
+    duration: "2.5 months",
+    image: "./images/img7.webp",
+  },
+  {
+    id: 6,
+    name: "Mern Stack Training",
+    duration: "3 months",
+    image: "./images/img8.webp",
+  },
+  {
+    id: 7,
+    name: "Python & Django Training",
+    duration: "2.5 months",
+    image: "./images/img9.webp",
+  },
+  {
+    id: 8,
+    name: "Flutter Framework Training",
+    duration: "2.5 months",
+    image: "./images/img10.webp",
+  },
+];
+
+let htmlPrint = "";
+courses.map((course) => {
+  htmlPrint += `<div class="col-sm-12 col-md-6 col-lg-3">
+    <div class="effect">
+      <img class="img img-fluid" src="${course.image}" alt="" />
+    </div>
+    <div>
+      <h6>${course.name}</h6>
+      <p>
+        <span>Duration:</span>
+        <strong>${course.duration}</strong>
+      </p>
+    </div>
+  </div>`;
+});
+
+document.getElementById("courses1").innerHTML = htmlPrint
