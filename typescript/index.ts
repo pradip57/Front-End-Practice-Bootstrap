d18f1cc4baec2ca3b7dc7ae75a9700ffcdf48c60
Pradip <dellpidarp57@gmail.com>
2024-05-15 12:47:07 +0545

    type declare in ts basic

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,2 +1,39 @@
 let name1:string = "Pradip Sapkota"
 
+
+type userRole = "admin"|"seller"|"staff"
+
+interface userObject {
+
+    name:string,
+    email:string,
+    address:string,//compulsory
+    role:userRole   
+    status?:string //optional
+}
+
+const object:userObject = {
+    name:"Hari",
+    email:"harikarki@gmail.com",
+    address:"Sitapaila",
+    role:"admin"
+    
+}
+
+//same also  for normal function as arrow function
+
+const fuc1 = (a:number,b:number):number =>{
+
+return a+b
+
+}
+
+
+//async await function
+
+const func2 = async ():Promise<string> =>{
+
+    return ""
+}
+
+
