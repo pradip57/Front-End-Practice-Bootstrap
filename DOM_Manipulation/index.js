a7b680c6dfa6789591c7fea2726ff3cd68dd4174
Pradip <dellpidarp57@gmail.com>
2024-05-15 12:38:47 +0545

    typescript basics started for project

--- DOM_Manipulation/index.js
+++ DOM_Manipulation/index.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   const write = document.getElementById("rewrite");
   write.addEventListener("keyup", (e) => {
+    console.log(e)
     const value1 = e.target.value
     document.getElementById("rewrite1").innerHTML = value1;
   });
---
+++ typescript/index.ts
@@ -0,0 +1,2 @@
+let name1:string = "Pradip Sapkota"
+
