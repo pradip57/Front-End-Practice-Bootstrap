bfe93fb8a84591855b3b8bba5447c557de36a403
Pradip <dellpidarp57@gmail.com>
2024-05-19 08:28:19 +0545

    tailwind components usedto

--- frontend/react-8am/src/main.tsx
+++ frontend/react-8am/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import LandingPage from "./pages/landing/landing.page";
+// import LandingPage from "./pages/landing/landing.page";
+import TailwindPracticePage from "./pages/practice_tailwind/tailwind_practice";
 
 const htmlRoot: HTMLElement = document.getElementById("root") as HTMLElement;
 
@@ -10,6 +11,7 @@ const rootElement = ReactDOM.createRoot(htmlRoot);
 //jsx
 rootElement.render(
   <React.StrictMode>
-    <LandingPage name="Pradip Sapkota" email="pradip57@gmail.com" address="Sitapaila" />
+    {/* <LandingPage name="Pradip Sapkota" email="pradip57@gmail.com" address="Sitapaila" /> */}
+    <TailwindPracticePage />
   </React.StrictMode>
 );
