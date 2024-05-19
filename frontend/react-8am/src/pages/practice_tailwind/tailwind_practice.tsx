527719179e9cadda359e82c1f49e56ccfce660f3
Pradip <dellpidarp57@gmail.com>
2024-05-19 08:44:08 +0545

    practice ongoing

--- frontend/react-8am/src/pages/practice_tailwind/tailwind_practice.tsx
+++ frontend/react-8am/src/pages/practice_tailwind/tailwind_practice.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import myImage from "../../assets/myimg.jpg"
 
 const TailwindPracticePage = (): ReactNode => {
   return (
@@ -121,7 +122,7 @@ const TailwindPracticePage = (): ReactNode => {
                     <span className="sr-only">Open user menu</span>
                     <img
                       className="h-8 w-8 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src={myImage}
                       alt=""
                     />
                   </button>
