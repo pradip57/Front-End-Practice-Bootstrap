0bee9b9734ab32f15a607e531ded2bd33b42919e
Pradip <dellpidarp57@gmail.com>
2024-05-23 21:18:59 +0545

    learning react

--- frontend/react-8am/src/pages/practice_tailwind/tailwind_practice.tsx
+++ frontend/react-8am/src/pages/practice_tailwind/tailwind_practice.tsx
@@ -228,7 +228,7 @@ const TailwindPracticePage = (): ReactNode => {
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              lorem cupidatat commodo.  sunt amet fugiat veniam occaecat
               fugiat aliqua.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
